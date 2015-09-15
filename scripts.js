270071ef36a4617bbed160a548ff97ae2c9fd180
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-09-15 14:11:43 -0700

    add test

--- scripts.js
+++ scripts.js
@@ -2,12 +2,21 @@ function Operator() {
     this.hey = 'hey';
 }
 
-function Add() {
-    this.yo = 'yo';
+function Add(x) {
+    this.act = function(n) {
+        return n + x;
+    };
+    this.toString = function() {
+        return '+' + x;
+    };
 }
 Add.prototype = new Operator();
 
 $(function() {
-    var add = new Add();
-    console.log('sup', add.yo, add.hey);
+    var add5 = new Add(5);
+    var number = 4;
+    console.log(number);
+    setInterval(function() {
+        console.log(number, '' + add5, number = add5.act(number));
+    }, 1000);
 });
