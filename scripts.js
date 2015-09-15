15d62ba6fca077b465b3539d64316e839f8b1b73
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-09-15 14:16:08 -0700

    reverse

--- scripts.js
+++ scripts.js
@@ -1,7 +1,3 @@
-function Operator() {
-    this.hey = 'hey';
-}
-
 function Add(x) {
     this.act = function(n) {
         return n + x;
@@ -9,8 +5,22 @@ function Add(x) {
     this.toString = function() {
         return '+' + x;
     };
+    this.reverse = function() {
+        return this.__reverse || (this.__reverse = new Subtract(x));
+    }
 }
-Add.prototype = new Operator();
+
+function Subtract(x) {
+    this.act = function(n) {
+        return n - x;
+    };
+    this.toString = function() {
+        return '-' + x;
+    };
+    this.reverse = function() {
+        return this.__reverse || (this.__reverse = new Add(x));
+    }
+}
 
 $(function() {
     var add5 = new Add(5);
@@ -18,5 +28,6 @@ $(function() {
     console.log(number);
     setInterval(function() {
         console.log(number, '' + add5, number = add5.act(number));
+        console.log(number, '' + add5.reverse(), number = add5.reverse().act(number));
     }, 1000);
 });
