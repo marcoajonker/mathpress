dbd2ebb287a5bc43bb22067ba166370df3581c51
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-09-15 14:26:38 -0700

    stupid version

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+var KEY_LEFT = 37;
+var KEY_UP = 38;
+var KEY_RIGHT = 39;
+var KEY_DOWN = 40;
+
 function Add(x) {
     this.act = function(n) {
         return n + x;
@@ -7,7 +12,7 @@ function Add(x) {
     };
     this.reverse = function() {
         return this.__reverse || (this.__reverse = new Subtract(x));
-    }
+    };
 }
 
 function Subtract(x) {
@@ -19,15 +24,31 @@ function Subtract(x) {
     };
     this.reverse = function() {
         return this.__reverse || (this.__reverse = new Add(x));
-    }
+    };
 }
 
 $(function() {
-    var add5 = new Add(5);
-    var number = 4;
-    console.log(number);
-    setInterval(function() {
-        console.log(number, '' + add5, number = add5.act(number));
-        console.log(number, '' + add5.reverse(), number = add5.reverse().act(number));
-    }, 1000);
+    var number = 1;
+    var operations = {};
+    operations[KEY_LEFT]  = new Subtract(4);
+    operations[KEY_UP]    = new Add(1);
+    operations[KEY_RIGHT] = new Add(2);
+    operations[KEY_DOWN]  = new Subtract(3);
+
+    function refresh_display() {
+        $('#me').text(number);
+        $('#left').text(operations[KEY_LEFT]);
+        $('#up').text(operations[KEY_UP]);
+        $('#right').text(operations[KEY_RIGHT]);
+        $('#down').text(operations[KEY_DOWN]);
+    }
+
+    refresh_display();
+    $(document).on('keydown', function(e) {
+        if (!operations[e.which]) {
+            return;
+        }
+        number = operations[e.which].act(number);
+        refresh_display();
+    });
 });
