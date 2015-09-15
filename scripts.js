a2b8fc089b9f2bb08bd334fda4c78c3c96f2ce77
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-09-15 15:11:06 -0700

    moving, kind of

--- scripts.js
+++ scripts.js
@@ -1,14 +1,3 @@
-var KEY_LEFT = 37;
-var KEY_UP = 38;
-var KEY_RIGHT = 39;
-var KEY_DOWN = 40;
-var KEY_SHIFT = 16;
-var opposites = {};
-opposites[KEY_LEFT] = KEY_RIGHT;
-opposites[KEY_UP] = KEY_DOWN;
-opposites[KEY_RIGHT] = KEY_LEFT;
-opposites[KEY_DOWN] = KEY_UP;
-
 function Add(x) {
     this.act = function(n) {
         return n + x;
@@ -33,7 +22,66 @@ function Subtract(x) {
     };
 }
 
+var KEY_LEFT = 37;
+var KEY_UP = 38;
+var KEY_RIGHT = 39;
+var KEY_DOWN = 40;
+/*
+var KEY_SHIFT = 16;
+var opposites = {};
+opposites[KEY_LEFT] = KEY_RIGHT;
+opposites[KEY_UP] = KEY_DOWN;
+opposites[KEY_RIGHT] = KEY_LEFT;
+opposites[KEY_DOWN] = KEY_UP;
+*/
+var NAMESPACE = 'mathpress';
+
 $(function() {
+    var viewport = $('#viewport');
+    var space = { '-1': { '-1': true, 0: true, 1: true },
+                  0:    { '-1': true, 0: true, 1: true },
+                  1:    { '-1': true, 0: true, 1: true } };
+    var position = { y: 0, x: 0 };
+
+    function draw_space() {
+        for (var y = 0; y < 5; y++) {
+            for (var x = 0; x < 5; x++) {
+                var real_y = y + position.y - 2;
+                var real_x = x + position.x - 2;
+                $('#cell_' + y + '_' + x).toggleClass('wall', !space[real_y] || !space[real_y][real_x]);
+            }
+        }
+    }
+
+    $('#cell_2_2').addClass('me');
+    draw_space();
+    $(document).on('keydown', function(e) {
+        var destination = { y: position.y, x: position.x };
+        switch (e.which) {
+            case KEY_LEFT:
+                destination.x--;
+                break;
+            case KEY_UP:
+                destination.y--;
+                break;
+            case KEY_RIGHT:
+                destination.x++;
+                break;
+            case KEY_DOWN:
+                destination.y++;
+                break;
+            default:
+                return;
+        }
+        if (!space[destination.y] || !space[destination.y][destination.x]) {
+            return;
+        }
+        position.y = destination.y;
+        position.x = destination.x;
+        draw_space();
+    });
+
+    /*
     var number = 1;
     var reverse = false;
     var operations = {};
@@ -70,4 +118,5 @@ $(function() {
             return;
         }
     });
+    */
 });
