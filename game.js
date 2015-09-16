efcb189a5fcea07246d62e92f047ede407b1ad13
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-09-16 14:28:56 -0700

    moving some stuff into css

--- game.js
+++ game.js
@@ -161,7 +161,7 @@ $(function() {
 
     function position_area(area, cell) {
         var position = cell.position();
-        area.css('transform', 'translate3d(' + (($(window).width() - 120 + 2) / 2 - position.left) + 'px, ' +
-                                               (($(window).height() - 120 + 2) / 2 - position.top) + 'px, 0)');
+        area.css('transform', 'translate3d(' + ($(window).width() / 2 - position.left) + 'px, ' +
+                                               ($(window).height() / 2 - position.top) + 'px, 0)');
     }
 });
